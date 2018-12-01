3d669cf214c099ad1337e4ea967ac8c2e5d75f49
DimaDK <DimaKrutDK@ya.ru>
2018-12-01 20:18:05 +0300

    chore: remove all unrelated to bug stuff

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import main from './main'
-
-async function index() {
-    const response = await main()
-    return response
-}
-
-export default index
--- src/main.js
+++
@@ -1,9 +0,0 @@
-function main() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(true)
-        }, 300)
-    })
-}
-
-export default main
